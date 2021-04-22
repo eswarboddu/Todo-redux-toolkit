acd3c967e54f3f52f6b7ffd6633710e50f7ff618
Eswar Rao <eswarboddu1995@gmail.com>
2021-04-23 09:43:30 +1000

    Second Commit

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -65,13 +65,7 @@ export const deleteTodoAsync = createAsyncThunk(
 
 const todoSlice = createSlice({
   name: "list",
-  initialState: [
-    { id: 1, title: "todo1", completed: false },
-    { id: 2, title: "todo2", completed: false },
-    { id: 3, title: "todo3", completed: true },
-    { id: 4, title: "todo4", completed: false },
-    { id: 5, title: "todo5", completed: false },
-  ],
+  initialState: [],
   reducers: {
     addTodo: (state, action) => {
       const newTodo = {
